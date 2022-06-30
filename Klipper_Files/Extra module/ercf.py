701d38a62d82401237b910eb9194e5e10c37e446
EtteGit <35068611+EtteGit@users.noreply.github.com>
2022-06-30 08:37:45 +0200

    Security measure for the no toolhead sensor branch

--- Klipper_Files/Extra module/ercf.py
+++ Klipper_Files/Extra module/ercf.py
@@ -339,15 +339,17 @@ class Ercf:
         homing_move = gcmd.get_int('HOMING', 0, minval=0, maxval=1)
         unknown_state = gcmd.get_int('UNKNOWN', 0, minval=0, maxval=1)
         req_length = gcmd.get_float('LENGTH', 1200.)
+        no_th = gcmd.get_int('NOTH', 0)
         num_moves = gcmd.get_int('MOVES', 1)
         self.toolhead.wait_moves()
         # Do not unload if filament is still in the toolhead
-        sensor = self.printer.lookup_object(
-                    "filament_switch_sensor toolhead_sensor")
-        if bool(sensor.runout_helper.filament_present):
-            self.gcode.respond_info(
-                "Unable to unload filament while still in extruder")
-            return
+        if no_th == 0:
+            sensor = self.printer.lookup_object(
+                        "filament_switch_sensor toolhead_sensor")
+            if bool(sensor.runout_helper.filament_present):
+                self.gcode.respond_info(
+                    "Unable to unload filament while still in extruder")
+                return
         # i.e. long move that will be fast and iterated using the encoder
         if req_length > self.LONG_MOVE_THRESHOLD: 
             req_length = req_length - buffer_length
